c5241dafbdd8b8d163decd9f98fdf0b16c0a20bc
magdalena-szkaradek <szkaradek.magda@gmail.com>
2018-12-03 13:32:31 +0100

    Add PathAlgorithm

---
+++ src/graphExample/PathAlgorithm.java
@@ -0,0 +1,23 @@
+package graphExample;
+
+public class PathAlgorithm implements PathInterface{
+    @Override
+    public Graph buildGraph(String fileName) {
+        return null;
+    }
+
+    @Override
+    public String decompress(Graph graph, int node, String code) {
+        return null;
+    }
+
+    @Override
+    public String findPath(Graph graph, int beginingNode, int destinationNode) {
+        return null;
+    }
+
+    @Override
+    public String getPathString(Graph graph, int beginingNode, int destinationNode) {
+        return null;
+    }
+}
